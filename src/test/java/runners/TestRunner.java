40f56827b91956b64f15dc24309f669a71130b0b
Sachin Soni <sonisachin@google.com>
2021-10-07 18:00:32 +0530

    moving test code to main

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,9 +8,9 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features={"src/test/resources/Test"},
-		glue= {"stepsDesign","stepsLogging","stepsSecurity"},
+		glue= {"stepsDesign"},
 		monochrome=true, 
-				plugin={"pretty","html:target/cucumber-html-report","json:target/cucumber-reports/cucumber.json","junit:target/cucumber-reports/cucumber.xml","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+				plugin={"pretty","html:target/cucumber-html-report","json:target/cucumber-reports/cucumber.json","junit:target/cucumber-reports/cucumber.xml"}
 
 				)
 
