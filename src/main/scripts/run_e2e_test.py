9e4fe73e6dcaf1786124902e0a0e2a997aea0456
itsankit-google <itsankitjain@google.com>
2023-07-19 19:46:19 +0000

    bump gcs connector version

--- src/main/scripts/run_e2e_test.py
+++ src/main/scripts/run_e2e_test.py
@@ -47,8 +47,8 @@ z = zipfile.ZipFile(io.BytesIO(r.content))
 z.extractall("./sandbox")
 
 print("Installing gcs connector jar")
-gcs_jar_url = "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop2-2.2.5.jar"
-gcs_jar_fname = f"sandbox/{sandbox_dir}/lib/gcs-connector-hadoop2-2.2.5.jar"
+gcs_jar_url = "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop2-2.2.9.jar"
+gcs_jar_fname = f"sandbox/{sandbox_dir}/lib/gcs-connector-hadoop2-2.2.9.jar"
 urllib.request.urlretrieve(gcs_jar_url, gcs_jar_fname)
 
 print("Start the sandbox")
