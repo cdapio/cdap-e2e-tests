b448578c6061657fef58612943d8a1f372cab136
Aryan-Verma <aryanverma@google.com>
2023-08-31 13:55:08 +0530

    Fix chrome driver issue

--- src/main/java/io/cdap/e2e/utils/SeleniumDriver.java
+++ src/main/java/io/cdap/e2e/utils/SeleniumDriver.java
@@ -16,9 +16,9 @@
 
 package io.cdap.e2e.utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.HttpCommandExecutor;
 import org.openqa.selenium.remote.SessionId;
@@ -43,7 +43,7 @@ public class SeleniumDriver {
   private static ChromeDriver chromeDriver;
 
   SeleniumDriver() throws IOException {
-    WebDriverManager.chromedriver().setup();
+    ChromeDriverService service = new ChromeDriverService.Builder().build();
     ChromeOptions chromeOptions = new ChromeOptions();
     chromeOptions.addArguments("--no-sandbox");
     chromeOptions.addArguments("--disable-setuid-sandbox");
@@ -52,7 +52,7 @@ public class SeleniumDriver {
     chromeOptions.addArguments("--disable-gpu");
     chromeOptions.addArguments("--disable-dev-shm-usage");
     chromeOptions.addArguments("--disable-features=VizDisplayCompositor");
-    chromeDriver = new ChromeDriver(chromeOptions);
+    chromeDriver = new ChromeDriver(service, chromeOptions);
     chromeDriver.manage().window().maximize();
     HttpCommandExecutor executor = (HttpCommandExecutor) chromeDriver.getCommandExecutor();
     url = executor.getAddressOfRemoteServer();
